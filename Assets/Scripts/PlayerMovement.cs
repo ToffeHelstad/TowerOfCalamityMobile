d71119b67f280f32c3dc3212460d342a434abbfe
Vetlish <vetlewintersbusiness@gmail.com>
2024-04-04 15:05:18 +0200

    CONTROLS
    Jadaaaaa
    
    også lagt til noen mapper med scripts og scenes osv

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -11,11 +12,19 @@ public class PlayerMovement : MonoBehaviour
 
     Vector2 movement;
 
+    //[SerializeField] private InputActionReference moveInput;
+    public FloatingJoystick joystick;
+
     // Update is called once per frame
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        //movement.x = Input.GetAxisRaw("Horizontal");
+        //movement.y = Input.GetAxisRaw("Vertical");
+
+        movement.x = joystick.Horizontal;
+        movement.y = joystick.Vertical;
+
+        //movement = moveInput.action.ReadValue<Vector2>();
     }
 
     void FixedUpdate()
